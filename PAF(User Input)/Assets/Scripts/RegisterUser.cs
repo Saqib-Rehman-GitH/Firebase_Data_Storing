032606033d8bd3f7ae9f7adaecb6eb13156887cc
Saqib Rehman <sa.reh0071@gmail.com>
2020-03-06 21:42:23 +0500

    first Commit

---
+++ PAF(User Input)/Assets/Scripts/RegisterUser.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Security;
+using Models;
+using Proyecto26;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisterUser : MonoBehaviour
+
+
+{
+    public InputField userName;
+    public InputField userEmail;
+
+    public InputField userPassword;
+
+    
+    public InputField checkPassword;
+    public InputField checkEmail;
+
+
+    public string uCheckEmail;
+    public string uCheckPassword;
+    public string sUserName;
+    public string sUserEmail;
+    public string sUserPassword;
+    
+    
+    
+    
+    public void onRegister() {
+sUserEmail = userEmail.text; 
+sUserName = userName.text;
+sUserPassword= userPassword.text;
+
+
+
+Debug.Log("UserName == " + sUserName );
+
+
+Debug.Log("User email  == " + sUserEmail );
+
+
+Debug.Log("Userpassword == " + sUserPassword );
+
+postToDatabase();
+
+    }
+
+    void postToDatabase()
+    {
+        RegisterUser r2 = new RegisterUser();
+        
+        r2.sUserEmail = userEmail.text; 
+        r2.sUserName = userName.text;
+        r2.sUserPassword= userPassword.text;
+        User u3 = new User(r2);
+      
+        RestClient.Post("https://pafappdatabase.firebaseio.com/"+u3.userEmail+".json" ,u3 );
+
+
+    }
+
+    // void methodCheckUser()
+
+   // {
+
+      //  uCheckEmail = checkEmail.text;
+       // 
+       // uCheckPassword = checkPassword.text;
+
+        
+       // retreiveFromDatabase(uCheckEmail).sUserName.ToString();
+        
+       // retreiveFromDatabase(uCheckEmail).sUserEmail.ToString();
+ 
+       // retreiveFromDatabase(uCheckEmail).sUserPassword.ToString();
+
+
+       // }
+//
+
+
+   // void  retreiveFromDatabase(string dataBaseEmail )
+   // {
+
+   //     RestClient.Get<User>("https://pafappdatabase.firebaseio.com/"+dataBaseEmail+".json").Then(response => { return response; });
+  //  }
+
+
+
+
+
+
+
+}
+
+
---
+++ PAF(User Input)/Assets/Scripts/User.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User
+{
+    
+    
+     public string userName ;
+    
+     public string userEmail ;
+    
+     public string userPassword ;
+
+
+     public User(RegisterUser ri)
+     {
+
+          userName = ri.sUserName;
+
+          userEmail = ri.sUserEmail;
+
+          userPassword = ri.sUserPassword;
+
+
+     }
+
+}
